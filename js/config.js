0a14c4ccae5525465fd155ece97239016853bd25
Steve W <stevewardle@proton.me>
2026-09-23 12:44:28 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_QH3qb5U9M', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6184koui831m49gni5purvckef', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'kqvdlp2e3rqr4kmi93kb0t3ne', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
